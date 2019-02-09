62ad5785a0992e4b860f1ed1950620c65dcb893e
Nimfius <alexey.pavlovs97@gmail.com>
2019-02-09 17:43:06 +0200

    Добавьте файлы проекта.

---
+++ coffee_machine/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace coffee_machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 219);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Espresso";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(91, 263);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Latte";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(335, 263);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 43);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Macchiato";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(335, 219);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(165, 43);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Americano";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(289, 68);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Maximum = 20;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(240, 35);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Value = 20;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(289, 157);
+            this.progressBar2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar2.Maximum = 20;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(240, 35);
+            this.progressBar2.TabIndex = 8;
+            this.progressBar2.Value = 20;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(71, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Quantity of coffee grains";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(71, 113);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Quantity of milk";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(289, 113);
+            this.progressBar3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar3.Maximum = 20;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(240, 35);
+            this.progressBar3.TabIndex = 11;
+            this.progressBar3.Value = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(71, 157);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Quantity of water";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(587, 91);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 62);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Add all\r\ncomponents";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(238, 19);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 24);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(242, 316);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 345);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 200;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 200;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(759, 666);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.ToolTip toolTip4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ coffee_machine/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coffee_machine
+{
+    public partial class Form1 : Form
+    {
+        List<Button> buttons = new List<Button>();
+
+        int y = 140;
+        int y2 = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = "Please, select a drink!";
+            buttons.Add(button1);
+            buttons.Add(button3);
+            buttons.Add(button4);
+            buttons.Add(button7);
+            formGraphics= pictureBox1.CreateGraphics(); 
+            water= pictureBox1.CreateGraphics(); 
+            milk= pictureBox1.CreateGraphics(); 
+            milk_foam= pictureBox1.CreateGraphics(); 
+        }
+
+        private void disable_buttons()
+        {
+            buttons.ForEach(delegate (Button b) {
+                b.Enabled = false;
+        });
+        }
+
+        private void enable_buttons()
+        {
+            buttons.ForEach(delegate (Button b) {
+                b.Enabled = true;
+            });
+        }
+
+        private bool check_quantity()
+        {
+            if (progressBar1.Value == 0)
+            {
+                disable_buttons();
+                label4.Text ="Please, add coffee!";
+                return false;
+            }
+            if (progressBar3.Value == 0)
+            {
+                disable_buttons();
+                label4.Text = "Please, add milk!";
+                return false;
+            }
+            if (progressBar2.Value == 0)
+            {
+                disable_buttons();
+                label4.Text = "Please, add water!";
+                return false;
+            }
+            if (progressBar2.Value < 3)
+            {
+                button7.Enabled = false;
+            }
+            if (progressBar3.Value < 3)
+            {
+                button3.Enabled = false;
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(button1, "Black coffee");
+            toolTip2.SetToolTip(button7, "Black coffee with water");
+            toolTip3.SetToolTip(button3, "Black coffee with milk and milk foam");
+            toolTip4.SetToolTip(button4, "Black coffee with milk");
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 20;
+            progressBar2.Value = 20;
+            progressBar3.Value = 20;
+            enable_buttons();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (check_quantity())
+            {
+                progressBar1.Value--;
+                progressBar2.Value--;
+                check_quantity();
+                disable_buttons();
+                draw_cup();
+                timer1.Enabled = true;
+            }
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (check_quantity())
+            {
+                progressBar1.Value--;
+                progressBar2.Value= progressBar2.Value-3;
+                check_quantity();
+                disable_buttons();
+                draw_cup();
+                timer2.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (check_quantity())
+            {
+                progressBar1.Value--;
+                progressBar3.Value= progressBar3.Value - 3;
+                check_quantity();
+                disable_buttons();
+                draw_cup();
+                timer3.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (check_quantity())
+            {
+                progressBar1.Value--;
+                progressBar2.Value--;
+                progressBar3.Value--;
+                check_quantity();
+                disable_buttons();
+                draw_cup();
+                timer4.Enabled = true;
+            }
+        }
+
+        private void draw_cup()
+        {
+            label4.Text = "Please, wait - your drink is preparing";
+            Graphics cup = pictureBox1.CreateGraphics();
+            Pen p = new Pen(Color.Black, 3);
+            Point p1 = new Point(10, 10);
+            Point p2 = new Point(10, 150);
+            cup.DrawLine(p, p1, p2);
+            p1 = new Point(10, 150);
+            p2 = new Point(140, 150);
+            cup.DrawLine(p, p1, p2);
+            p1 = new Point(140, 150);
+            p2 = new Point(140, 10);
+            cup.DrawLine(p, p1, p2);
+            cup.DrawArc(new Pen(new SolidBrush(Color.Black), 10),110,60,60,50, 270, 180);
+            cup.Dispose();
+            p.Dispose();
+        }
+
+        private void draw_coffee()
+        {
+            formGraphics.FillRectangle(Brush, new Rectangle(12, y, 127, y2));
+            y -= 10;
+            y2 += 10;
+            if (y == 90)
+                y2 = 10;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (is_cup_ready)
+            {
+                pictureBox1.Image = null;
+                is_cup_ready = false;
+                label4.Text = "please, select a drink!";
+                enable_buttons();
+            }
+        }
+        
+        SolidBrush Brush = new SolidBrush(Color.Brown);
+        SolidBrush Brush_water = new SolidBrush(Color.Blue);
+        SolidBrush Brush_milk = new SolidBrush(Color.White);
+        SolidBrush Brush_milk_foam = new SolidBrush(Color.WhiteSmoke);
+        Graphics formGraphics, water, milk, milk_foam;
+
+        private void cup_is_ready()
+        {
+            y = 140;
+            y2 = 10;
+            is_cup_ready = true;
+            label4.Text = "Please, take your coffee!";
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (y < 20)
+            {
+                timer3.Enabled = false;
+                cup_is_ready();
+            }
+            else
+            {
+                if (y <= 40)
+                {
+                    water.FillRectangle(Brush_milk_foam, new Rectangle(12, y, 127, y2));
+                    y -= 10;
+                    y2 += 10;
+                }
+                else
+                {
+                    if (y <= 90)
+                    {
+                        water.FillRectangle(Brush_milk, new Rectangle(12, y, 127, y2));
+                        y -= 10;
+                        y2 += 10;
+                        if (y <= 40) y2 = 10;
+                    }
+                    else
+                    {
+                        draw_coffee();
+                    }
+                }
+
+                
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (y <= 90)
+            {
+                if (y < 70)
+                {
+                    timer4.Enabled = false;
+                    cup_is_ready();
+                }
+                else
+                {
+                    water.FillRectangle(Brush_milk_foam, new Rectangle(12, y, 127, y2));
+                    y -= 10;
+                    y2 += 10;
+                }
+
+            }
+            else
+            {
+                draw_coffee();
+            }
+        }
+
+        bool is_cup_ready = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (y < 100)
+            {
+                timer1.Enabled = false;
+                cup_is_ready();
+            }
+            else
+            {
+                formGraphics.FillRectangle(Brush, new Rectangle(12, y, 127, y2));
+                y -= 10;
+                y2 += 10;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (y <= 90)
+            {
+                if (y < 20)
+                {
+                    timer2.Enabled = false;
+                    cup_is_ready();
+                }
+                else
+                {
+                    water.FillRectangle(Brush_water, new Rectangle(12, y, 127, y2));
+                    y -= 10;
+                    y2 += 10;
+                }
+                
+            }
+            else
+            {
+                draw_coffee();
+            }
+        }
+    }
+}
